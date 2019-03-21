ea9c3cd86863cc8825d2de0c8464d0fbc84cd787
MartinVai <48659785+MartinVai@users.noreply.github.com>
2019-03-21 18:20:56 +0200

    lõpetab nüüd ära, kui 'lõpp' asemel midagi muud lisada peale sõna complete-imist

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,14 @@ public class Main {
         System.out.println("Tere! Uue mängu jaoks kirjutage \"Uusmäng\"" +
                 ", lõpetamiseks kirjutage \"lõpp\"");
         Mäng mäng = new Mäng(1);
+        int i = 0;
         //Mängu tsükkel
         while(true){
             //System.out.println("Tsüklialgus");
             Scanner sc = new Scanner(System.in);
             String sisend = sc.nextLine().toUpperCase();
             //Programmi peatamine
-            if(sisend.equals("LÕPP")||sisend.toUpperCase().equals("LOPP")){
+            if(sisend.equals("LÕPP")||sisend.toUpperCase().equals("LOPP") | i==1){
                 System.out.println("Lõpetan mängu");
                 break;
             }
@@ -42,6 +43,7 @@ public class Main {
             // Lahenduse kontrollimine
             String arvamine = mäng.getArvamine().replace(" ","");
             if(arvamine.equals(mäng.getSõna())||sisend.equals(arvamine)){
+                i+=1;
                 Tulemus tulemus = new Tulemus(mäng.getSõna(), mäng.ArvamisteSumma()-1));
                 tulemus.lisatulemus();
                 System.out.println("Palju õnne, lahendasite sõna ära, kokku kulus " + (mäng.getArvamised()-1) + " arvamist.");
